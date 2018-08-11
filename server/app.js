0fcb9269d7cc239f4de190205baaf305eeb91e1f
Brice <brykou@gmail.com>
2018-08-11 09:55:15 +0200

    Create POST /files route
    Create a POST /files endpoint to upload a new file.
    File upload is handled using Multer.

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const multer = require("multer");
 const cors = require("cors");
 
 const port = 3001;
@@ -9,6 +10,16 @@ const corsOptions = {
   origin: "http://localhost:3000",
   methods: ["GET", "HEAD", "POST", "DELETE"]
 };
+
+const storage = multer.diskStorage({
+  destination: uploadFolder,
+  filename(req, file, cb) {
+    cb(null, `${new Date().getTime()}-${file.originalname}`);
+  }
+});
+const upload = multer({
+  storage: storage
+});
 
 const app = express();
 app.use(cors(corsOptions));
@@ -20,6 +31,13 @@ app.get("/files", (req, res) => {
       fileList.push(file);
     });
     res.send(fileList);
+  });
+});
+
+app.post("/files", upload.single("fileToUpload"), (req, res) => {
+  res.send({
+    id: req.file.filename,
+    name: req.file.originalname
   });
 });
 
